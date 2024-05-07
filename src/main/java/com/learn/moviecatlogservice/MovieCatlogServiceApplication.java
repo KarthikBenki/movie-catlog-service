1502e7745c989f3273482391c6a4e26048a68fdb
KarthikBenki <karthikmc007@gmail.com>
2024-05-07 09:11:45 +0530

    usage of web client to call the api

--- src/main/java/com/learn/moviecatlogservice/MovieCatlogServiceApplication.java
+++ src/main/java/com/learn/moviecatlogservice/MovieCatlogServiceApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatlogServiceApplication {
+
+	@Bean
+	public WebClient.Builder getWebClient(){
+		return  WebClient.builder();
+	}
 
 	//create a common instance of RestTemplate using @Bean
 	@Bean
--- src/main/java/com/learn/moviecatlogservice/controller/MovieCatalogController.java
+++ src/main/java/com/learn/moviecatlogservice/controller/MovieCatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +22,9 @@ public class MovieCatalogController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
 
     @Value("${movie.info.uri}")
     private String movie_info_uri;
@@ -34,8 +38,16 @@ public class MovieCatalogController {
         return ratings
                 .stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject(movie_info_uri+rating.getMovieId(), Movie.class);
-                 return    new CatalogItem(movie.getName(), "It is a kannada Movie", rating.getRating());
+//                    Movie movie = restTemplate.getForObject(movie_info_uri+rating.getMovieId(), Movie.class);
+
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri(movie_info_uri + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+
+                    return    new CatalogItem(movie.getName(), "It is a kannada Movie", rating.getRating());
                 })
                 .collect(Collectors.toList());
 
