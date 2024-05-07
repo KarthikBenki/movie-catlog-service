71dc195ecdd203decf6400c40d1ba19e33aae5b7
KarthikBenki <karthikmc007@gmail.com>
2024-05-07 08:38:42 +0530

    create a bean and use it from dependency injection

--- src/main/java/com/learn/moviecatlogservice/MovieCatlogServiceApplication.java
+++ src/main/java/com/learn/moviecatlogservice/MovieCatlogServiceApplication.java
@@ -2,9 +2,17 @@ package com.learn.moviecatlogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatlogServiceApplication {
+
+	//create a common instance of RestTemplate using @Bean
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return  new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatlogServiceApplication.class, args);
--- src/main/java/com/learn/moviecatlogservice/controller/MovieCatalogController.java
+++ src/main/java/com/learn/moviecatlogservice/controller/MovieCatalogController.java
@@ -3,6 +3,7 @@ package com.learn.moviecatlogservice.controller;
 import com.learn.moviecatlogservice.model.CatalogItem;
 import com.learn.moviecatlogservice.model.Movie;
 import com.learn.moviecatlogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +18,11 @@ import java.util.stream.Collectors;
 @RequestMapping("catalog")
 public class MovieCatalogController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")  String userId) {
-        RestTemplate restTemplate = new RestTemplate();
         List<Rating> ratings = Arrays.asList(new Rating("1234",5),
                 new Rating("5678",2));
 
